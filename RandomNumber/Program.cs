63af793a9ba3879a0066b4077f7941334912ad3c
Cryslin Sinimäe <csinimae@tktk.ee>
2024-03-26 03:00:12 +0200

    Week4Loops

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+
+int randomSum = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    int myRandomNum = rnd.Next(0, 11); // 0-10
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {randomSum}");
